cfdcfaf66a2e6a5c8500898f6f3499c17adc1d73
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-02-16 21:01:14 +0200

    Add files via upload

---
+++ Log4Net/Code/Log4NetExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Log4NetExample
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log.Info("Hello world!");
+            Employee e = new Employee();
+        }
+        public class Employee
+        {
+            private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            public Employee()
+            {
+                log.Debug("new emplpoyee!");
+            }
+            public void BuyTicket()
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    log.Error("Buy ticket error " + ex.ToString());
+                }
+            }
+        }
+
+
+    }
+}
